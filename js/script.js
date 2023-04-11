c3b7777eb5566c6577150b24f49c3c0b3799097f
M-Andrade <46411226+M-Andrade@users.noreply.github.com>
2023-04-11 20:51:23 -0300

    Add btn reload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
 const gameOver = document.querySelector('.game-over');
+const star = document.querySelector('.star');
+let time = 1500;
 
 const jump = () => {
     mario.classList.add('jump');
@@ -15,7 +17,6 @@ const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-    const cloudsPosition = +window.getComputedStyle(clouds).right.replace('px', '');
 
     if (pipePosition <= 120 &&pipePosition > 0 && marioPosition < 80) {
 
@@ -25,18 +26,28 @@ const loop = setInterval(() => {
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        clouds.style.animation = 'none';
-        clouds.style.right = `${cloudsPosition}px`;
-
         mario.src = 'images/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
 
         gameOver.style.display = 'block';
 
-        clearInterval(loop);
-    }
+        star.style.display = 'block',
 
+        clearInterval(loop);
+    } /*else{ 
+        setTimeout(() => {
+            time -= 5;
+            console.log(`Valor atual: ${time}`);
+            pipe.style.animation = 'pipe-animation '+time+'ms infinite linear';
+        }, 1000)
+    }*/
 }, 10)
+
+const meuBotao = document.getElementById("btn-star");
+
+meuBotao.addEventListener("click", function() {
+    location.reload();
+});
 
 document.addEventListener('keydown', jump);
